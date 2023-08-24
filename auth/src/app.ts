b9aa116c2d3829e93ff6760b0ae645a6ba4df254
Max Alva <alva85@gmail.com>
2023-08-24 17:05:18 -0500

    Fix cors

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cookieSession from 'cookie-session';
+import cors from 'cors';
 import appRoutes from './routes';
 import { errorHandler } from './middlewares';
 import { NotFoundError } from './errors';
@@ -13,7 +14,17 @@ app.use( express.json() );
 app.use( cookieSession( {
     signed: false,
     secure: process.env.NODE_ENV !== 'test',
+    sameSite: 'none',
 } ) );
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    exposedHeaders: [ 'set-cookie' ],
+};
+
+app.use( cors( corsOptions ) );
+app.options( '*', cors( corsOptions ) );
 
 app.use( '/api', appRoutes );
 
