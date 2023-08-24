bb14e523bd025e65a8f51283f722f399efe2126a
Max Alva <alva85@gmail.com>
2023-08-24 08:14:00 -0500

    refactor app.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cookieSession from 'cookie-session';
+import appRoutes from './routes';
+import { errorHandler } from './middlewares';
+import { NotFoundError } from './errors';
+
+dotenv.config();
+
+const app = express();
+app.set( 'trust proxy', true );
+app.use( express.json() );
+app.use( cookieSession( {
+    signed: false,
+    secure: true,
+} ) );
+
+app.use( '/api', appRoutes );
+
+app.all( '*', ( req, res, next ) => {
+    throw new NotFoundError();
+} );
+
+app.use( errorHandler );
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,28 +1,5 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cookieSession from 'cookie-session';
-import appRoutes from './routes';
-import { errorHandler } from './middlewares';
-import { NotFoundError } from './errors/not-found-error';
 import { dbConnection } from './config/db';
-
-dotenv.config();
-
-const app = express();
-app.set( 'trust proxy', true );
-app.use( express.json() );
-app.use( cookieSession( {
-    signed: false,
-    secure: true,
-} ) );
-
-app.use( '/api', appRoutes );
-
-app.all( '*', ( req, res, next ) => {
-    throw new NotFoundError();
-} );
-
-app.use( errorHandler );
+import { app } from './app';
 
 dbConnection()
     .then( () => {
